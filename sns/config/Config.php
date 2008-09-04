e07f968b9351bbe8a09bd699e0099a403d160cc4
impetus.opensource <impetus.opensource@3b8d754b-4855-0410-b92d-916abc3eb79e>
2008-09-04 10:38:42 +0000

    Added Mysql select db for consistency

--- sns/config/Config.php
+++ sns/config/Config.php
@@ -7,5 +7,6 @@ $database_esprit_conn = "espritsns";
 $username_esprit_conn = "root";
 $password_esprit_conn = "";
 $esprit_conn = mysql_connect($hostname_esprit_conn, $username_esprit_conn, $password_esprit_conn) or trigger_error(mysql_error(),E_USER_ERROR); 
+mysql_select_db($database_esprit_conn,$esprit_conn);
 
 ?>
